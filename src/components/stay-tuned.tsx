718b802d9795216ff38e755389dad3c40563751d
henry <pye.henry@protonmail.com>
2024-06-26 15:55:11 -0400

    Or am i already here

--- src/components/stay-tuned.tsx
+++ src/components/stay-tuned.tsx
@@ -6,9 +6,10 @@ function StayTuned() {
   const glitch = useGlitch();
 
   return (
-    <h1 ref={glitch.ref} className="text-[32px]">
-      Coming Soon
-    </h1>
+    <div className="flex flex-col w-full justify-center items-center" ref={glitch.ref}>
+      <h1 className="text-[32px]">Coming Soon</h1>
+      <p>Or am I already here?</p>
+    </div>
   );
 }
 
